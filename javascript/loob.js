f25ccfaf45fa29f0eda448d81e6984b237bf9ac2
abdalmajeed-zeyad <abd.d@live.com>
2021-04-06 15:27:54 +0300

    a

--- javascript/loob.js
+++ javascript/loob.js
@@ -1,17 +1,156 @@
-for (let i = 0; i <19; i++) {
-if (i=7) {
-    break
-}    
-console.log('hello ');
-}
+"use strict";
+
+
 
 
 let pass= prompt('enter your password')
 let con= prompt('confirm your password')
 
-while (!pass===con) {
-    let pass= prompt('it is not matching, repeat the password')
-    let con =prompt('confirm the password again')
+
+while (!(pass===con)) {
+    pass = prompt('it is not matching, repeat the password')
+     con  = prompt('confirm the password again')
+    
+}
+console.log('your password is '+ pass);
+
+
+
+
+
+
+
+
+
+
+let Name= prompt('please enter your name');
+alert('Welcome ' + Name)
+
+let score = 0;
+
+let FavFood= prompt('do you think I am crazy  ?,  (yes(y) or no(n))');
+ FavFood=FavFood.toLowerCase()
+if ( FavFood ==='yes'||FavFood==='y'){ 
+  alert('great ')
+  score=score + 1 
+}
+else{
+  alert ('I am abdelmajed')
 }
 
-let array = [5,7,87,879,98,65]
+
+console.log(FavFood, "Correct");
+
+let soccor= prompt('do you think I am older than 18?,  (yes(y) or no(n))');
+soccor=soccor.toLowerCase()
+if ( soccor ==='yes'||soccor==='y'){ 
+  alert('that is right')
+  score=score + 1 
+
+}
+else{
+  alert ('I am 25')
+}
+
+
+console.log(soccor, "Correct");
+
+let age = prompt('do you think I love food  ?,  (yes(y) or no(n))');
+age=age.toLowerCase()
+if ( age ==='yes'||age==='y'){ 
+  alert('who does not love food ?')
+  score=score + 1 
+
+}
+else{
+  alert ('that is wrong answer')
+}
+
+
+console.log(age, "Correct");
+
+
+let travel = prompt('do you think that I like football?,  (yes(y) or no(n))');
+travel=travel.toLowerCase()
+if ( travel ==='yes'||travel==='y'){ 
+  alert('yes I love barcelona ')
+  score=score + 1 
+
+}
+else{
+  alert ('no I like football')
+}
+
+
+console.log(travel, "Correct");
+
+
+let sleep = prompt('am I  a good guy ?,  (yes(y) or no(n))');
+sleep=sleep.toLowerCase()
+if ( sleep ==='yes'||sleep==='y'){ 
+  alert('you are awesome  ')
+  score=score + 1 
+
+}
+else{
+  alert ('thank you ')
+}
+
+
+console.log(sleep, "Correct");
+
+
+
+
+let num = prompt('guess a number')
+num = parseInt(num)
+for (let i = 0; i <= 3; i++) {
+
+  if (num>55) {
+    alert('it is too high')
+    
+    }
+
+else if (num<45) {
+    alert('it is too low')
+  } 
+  
+else if (num == 50){
+        alert('you got it ')
+        score=score + 1 
+        break
+    }
+    else{
+    alert('you are close, try number with not more than 5 in defferance')
+    }
+
+  num = prompt('guess a number')
+}
+
+
+
+
+
+let array = ['apple','banana','orange']
+
+let fruit = prompt('guess a fruit')
+
+for (let i = 0; i < 5; i++) {
+
+if (fruit == array[0] || fruit== array[1] || fruit== array[2]){
+
+alert('amazing, you found it')
+alert('here are all the answers  '+array)
+score=score + 1 
+
+break 
+}
+
+fruit = prompt('try different fruit')
+
+}
+
+alert('this is your final score  ' + score)
+
+
+
